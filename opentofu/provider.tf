686d57b6c92a17f1245bdf32026e53f1a221d2d7
name <you@example.com>
2026-06-04 11:16:14 -0400

    Update Proxmox provider to use API token authentication

--- opentofu/provider.tf
+++ opentofu/provider.tf
@@ -8,11 +8,9 @@ terraform {
 }
 
 provider "proxmox" {
-  endpoint = var.virtual_environment_endpoint
-  insecure = true
-
-  username = "root@pam"
-  password = var.virtual_environment_password
+  endpoint  = var.virtual_environment_endpoint
+  api_token = var.virtual_environment_token
+  insecure  = true
 
   ssh {
     agent    = true
